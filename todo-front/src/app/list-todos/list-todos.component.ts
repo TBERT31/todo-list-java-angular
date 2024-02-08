5664a8c88a24e82199ceff68ea711344e4d66456
tbert31 <berteau.thomas@hotmail.fr>
2024-02-08 19:06:42 +0100

    todo is okay

--- todo-front/src/app/list-todos/list-todos.component.ts
+++ todo-front/src/app/list-todos/list-todos.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.css']
 })
-export class ListTodosComponent {
+export class ListTodosComponent implements OnInit{
+
+  todos = [
+    { id:1, description:"Learn to code with Angular" },
+    { id:2, description:"Learn to code with Spring Boot" },
+    { id:3, description:"Go to sport" },
+    { id:3, description:"Walk with dog (Kaya Kaïïï)" },
+  ];
+
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
