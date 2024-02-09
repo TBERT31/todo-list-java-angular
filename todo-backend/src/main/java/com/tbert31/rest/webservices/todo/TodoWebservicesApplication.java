f5d529a51a5a65908dd7c802c09b0b0babe08b41
tbert31 <berteau.thomas@hotmail.fr>
2024-02-09 07:32:17 +0100

    initialize backend springboot

---
+++ todo-backend/src/main/java/com/tbert31/rest/webservices/todo/TodoWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.tbert31.rest.webservices.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoWebservicesApplication.class, args);
+	}
+
+}
---
+++ todo-backend/src/test/java/com/tbert31/rest/webservices/todo/TodoWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbert31.rest.webservices.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
