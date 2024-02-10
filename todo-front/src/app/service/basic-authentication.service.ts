5d24ed860899197bc39800cffbb4eb41e6120667
tbert31 <berteau.thomas@hotmail.fr>
2024-02-10 19:11:37 +0100

    make frontend work with JWT token now

--- todo-front/src/app/service/basic-authentication.service.ts
+++ todo-front/src/app/service/basic-authentication.service.ts
@@ -34,6 +34,22 @@ export class BasicAuthenticationService {
     );
   }
 
+  executeJWTAuthenticationService(username:string, password:string){
+    return this.http.post<any>(
+      `${API_URL}/authenticate`, {
+        username,
+        password
+      }).pipe(
+        map(
+          data => {
+            sessionStorage.setItem(AUTHENTICATED_USER, username);
+            sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
+            return data;
+          }
+        )
+    );
+  }
+
   getAuthenticatedUser(){
     return sessionStorage.getItem(AUTHENTICATED_USER);
   }
